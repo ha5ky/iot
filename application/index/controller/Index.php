7de4f556d2a51770926da7b9d27e4d5006dfe760
张纯 <chun.zhang@seetatech.com>
2019-04-08 11:47:58 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,16 +5,16 @@ use think\Controller;
 
 class Index extends Controller
 {
-	// public function _initialize() {
-	// 	echo $this->redirect('Comm/_initialize');
-
-	// 	// echo 111;
-	// 	// die();
+	public function _initialize() {
+		echo $this->redirect('Comm/_initialize');
 
 		// echo 111;
 		// die();
 
-	// }
+		echo 111;
+		die();
+
+	}
 
 	public function index(){
 		return $this->fetch('index/ws');
